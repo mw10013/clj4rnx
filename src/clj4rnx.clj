869cdf3fb430cfe86a5b40ac8a2f0cb38066663c
Michael Wu <mw10013@gmail.com>
2011-04-26 23:27:24 -0400

    sandbox

--- src/clj4rnx.clj
+++ src/clj4rnx.clj
@@ -2,6 +2,7 @@
   (:require
    [clojure.string :as str]
    [clojure.java.io :as io]
+   [clojure.contrib.math :as math]
    [osc :as osc]))
 
 (osc/osc-debug true)
@@ -41,16 +42,6 @@
                 :sample-filename "/Users/mw/Documents/music/vengence/VENGEANCE ESSENTIAL CLUB SOUNDS vol-1/VEC1 Bassdrums/VEC1 Clubby/VEC1 BD Clubby 05.wav"})
   (reset-instr {:instr-idx 0 :plugin-name "Audio/Generators/VST/Sylenth1" :preset 83})
   (reset-instr {:instr-idx 0 :plugin-name "Audio/Generators/VST/Vanguard"})
-
-  (do
-    (ev "clj4rnx.instrument = renoise.song():instrument(1)")
-    (ev "clj4rnx.instrument:clear()")
-    (ev "clj4rnx.instrument.plugin_properties:load_plugin('')")
-    (ev "clj4rnx.instrument.name = 'instr'")
-    (ev "clj4rnx.sample = clj4rnx.instrument:sample(1)")
-    (ev "clj4rnx.sample_buffer = clj4rnx.sample.sample_buffer")
-    (ev "clj4rnx.sample_buffer:load_from('/Users/mw/Documents/music/vengence/VENGEANCE ESSENTIAL CLUB SOUNDS vol-1/VEC1 Bassdrums/VEC1 Clubby/VEC1 BD Clubby 01.wav')")
-    )
   )
 
 (defn reset-song [ctx]
@@ -61,29 +52,37 @@
     (ev "clj4rnx.song:delete_track_at(" 1 ")")
     (ev "clj4rnx.song:delete_instrument_at(" 1 ")"))
   
-  (doseq [idx (range 2 (inc (:patr-cnt ctx)))]
-       (ev "clj4rnx.sequencer:insert_sequence_at(" idx ", " idx ")"))
+  (doseq [idx (range 1 (inc (:patr-cnt ctx)))]
+    (when-not (= idx 1)
+      (ev "clj4rnx.sequencer:insert_sequence_at(" idx ", " idx ")"))
+    (ev "clj4rnx.pattern = clj4rnx.song:pattern(" idx ")")
+    (ev "clj4rnx.pattern.number_of_lines = " (* 4 *lpb*))
+    (ev "clj4rnx.pattern:clear()"))
   (doseq [_ (range (dec (:track-cnt ctx)))]
     (ev "clj4rnx.song:insert_track_at(" 1 ")"))
-  (doseq [_ (range (dec (:instr-cnt ctx)))]
+  (doseq [_ (range (dec (count (:instrs ctx))))]
     (ev "clj4rnx.song:insert_instrument_at(" 1 ")"))
-  (map-indexed (fn [idx m] (reset-instr (assoc m :instr-idx idx))) (:instrs ctx)))
+  (dorun (map-indexed (fn [idx m] (reset-instr (assoc m :instr-idx idx))) (:instrs ctx))))
 
 (comment
-  (reset-song {:patr-cnt 3 :track-cnt 2 :instr-cnt 2
-               :instrs [{:plugin-name "Audio/Generators/VST/Sylenth1" :preset 82}
+  (reset-song {:patr-cnt 3 :track-cnt 7
+               :instrs [{:sample-filename "/Users/mw/Documents/music/vengence/VENGEANCE ESSENTIAL CLUB SOUNDS vol-1/VEC1 Bassdrums/VEC1 Trancy/VEC1 BD Trancy 10.wav"}
+                        {:sample-filename "/Users/mw/Documents/music/vengence/VENGEANCE ESSENTIAL CLUB SOUNDS vol-1/VEC1 Snares/VEC1 Snare 031.wav"}
+                        {:sample-filename "/Users/mw/Documents/music/vengence/VENGEANCE ESSENTIAL CLUB SOUNDS vol-1/VEC1 Cymbals/VEC1 Close HH/VEC1 Cymbals  CH 11.wav"}
+                        {:sample-filename "/Users/mw/Documents/music/vengence/VENGEANCE ESSENTIAL CLUB SOUNDS vol-1/VEC1 Cymbals/VEC1 Open HH/VEC1 Cymbals  OH 001.wav"}
+                        {:sample-filename "/Users/mw/Documents/music/vengence/VENGEANCE ESSENTIAL CLUB SOUNDS vol-1/VEC1 Claps/VEC1 Clap 027.wav"}
+                        {:plugin-name "Audio/Generators/VST/Sylenth1" :preset 82}
                         {:plugin-name "Audio/Generators/VST/Sylenth1" :preset 83}]})
-  (reset-song {:patr-cnt 4 :track-cnt 10 :instr-cnt 5
-               :instrs [{:plugin-name "Audio/Generators/VST/Sylenth1" :preset 42}
-                        {:plugin-name "Audio/Generators/VST/Sylenth1" :preset 43}]})
-  (reset-song 5)
+  
+  (reset-song {:patr-cnt 3 :track-cnt 2 :instr-cnt 2
+               :instrs [{:sample-filename "/Users/mw/Documents/music/vengence/VENGEANCE ESSENTIAL CLUB SOUNDS vol-1/VEC1 Bassdrums/VEC1 Clubby/VEC1 BD Clubby 05.wav"}
+                        {:plugin-name "Audio/Generators/VST/Sylenth1" :preset 82}
+                        {:plugin-name "Audio/Generators/VST/Sylenth1" :preset 83}]})
   )
 
-(defn reset-patr [idx beat-cnt]
-  (ev "clj4rnx.pattern = renoise.song():pattern(" idx ")")
-  (ev "clj4rnx.pattern.number_of_lines = " (* beat-cnt *lpb*))
-;  (ev "clj4rnx.pattern:clear()")
-  )
+(defn set-patr [idx beat-cnt]
+  (ev "clj4rnx.pattern = clj4rnx.song:pattern(" (inc idx) ")")
+  (ev "clj4rnx.pattern.number_of_lines = " (* beat-cnt *lpb*)))
 
 (defn set-notes [trk-idx notes]
   (ev "clj4rnx.track = clj4rnx.pattern:track(" trk-idx ")")
@@ -92,39 +91,76 @@
     (let [l (inc (* t *lpb*))]
       (ev "clj4rnx.note_column = clj4rnx.track:line(" l "):note_column(1)")
       (ev "clj4rnx.note_column.note_value = " p)
-      (ev "clj4rnx.note_column.volume_value = " v)
+      (ev "clj4rnx.note_column.volume_value = " (math/round (* v 254)))
+      (ev "clj4rnx.note_column.instrument_value = " (dec trk-idx))
       (ev "clj4rnx.note_column = clj4rnx.track:line(" (int (+  l (* d *lpb*))) "):note_column(1)")
-      (ev "clj4rnx.note_column.note_value = " 120)
-      )))
+      (ev "clj4rnx.note_column.note_string = 'OFF'" ))))
 
-(defn set-notes- [trk-idx notes]
-  (let [trk (str "renoise.song():pattern(1):track(" trk-idx ")")]
-    (ev trk ":clear()")
-    (doseq [[t p v d] notes]
-      (let [l (inc (* t *lpb*))
-            nc (str trk ":line(" l "):note_column(1)")
-            nc-off (str trk ":line(" (int (+  l (* d *lpb*))) "):note_column(1)")]
-        (ev nc ".note_value = " p)
-        (ev nc ".volume_value = " v)
-        (ev nc-off ".note_value = " 120)))))
+; (defn xform-note [n key f & args] (assoc n key (apply f (n key) args)))
+
+(comment (defn repeat-beats [n-coll dur & args]
+           (let [times (if (first args) (range (first args)) (iterate inc 0))]
+             (mapcat (fn [t] (map #(alter-note %1 1 + %2) n-coll (repeat (* dur t)))) times))))
+
+(defn take-beats [n coll]  (take-while #(< (% 0) n) coll))
+
+(defn xform-time [n t-coll dur]
+  (map #(assoc %1 0 %2) (repeat n) (mapcat #(map + t-coll (repeat (* dur %))) (iterate inc 0))))
+
+(defn within-beats [l u & args]
+  (let [len (if (odd? (count args)) (last args))
+        coll (concat [[l u]] (partition 2 args))]
+    (fn [n]
+      (let [v (if len (mod (n 0) len) (n 0))]
+        (some #(and (<= (first %) v) (<= v (last %))) coll)))))
+
+(defn- set-patr-0 []
+  (set-patr 0 4)
+  (set-notes 1 (take-beats 8.0 (xform-time [0 48 0.8 0.5] [0.0] 1.0))))
+
+(defn- set-patr-1 []
+  (set-patr 1 4)
+  (set-notes 1 (take-beats 8.0 (xform-time [0 48 0.8 0.5] [0.0] 1.0)))
+  (set-notes 2 (take-beats 8.0 (xform-time [0 48 0.8 0.5] [1.0] 2.0))))
+
+(defn- set-patr-2 []
+  (set-patr 2 4)
+  (set-notes 1 (take-beats 8.0 (xform-time [0 48 0.8 0.5] [0.0] 1.0)))
+  (set-notes 2 (take-beats 8.0 (xform-time [0 48 0.8 0.5] [1.0] 2.0)))
+  (set-notes 3 (take-beats 8.0 (xform-time [0 48 0.8 0.25] [0.5] 1.0))))
+
+
+(defn demo []
+  (reset-song {:patr-cnt 3 :track-cnt 7
+               :instrs [{:sample-filename "/Users/mw/Documents/music/vengence/VENGEANCE ESSENTIAL CLUB SOUNDS vol-1/VEC1 Bassdrums/VEC1 Trancy/VEC1 BD Trancy 10.wav"}
+                        {:sample-filename "/Users/mw/Documents/music/vengence/VENGEANCE ESSENTIAL CLUB SOUNDS vol-1/VEC1 Snares/VEC1 Snare 031.wav"}
+                        {:sample-filename "/Users/mw/Documents/music/vengence/VENGEANCE ESSENTIAL CLUB SOUNDS vol-1/VEC1 Cymbals/VEC1 Close HH/VEC1 Cymbals  CH 11.wav"}
+                        {:sample-filename "/Users/mw/Documents/music/vengence/VENGEANCE ESSENTIAL CLUB SOUNDS vol-1/VEC1 Cymbals/VEC1 Open HH/VEC1 Cymbals  OH 001.wav"}
+                        {:sample-filename "/Users/mw/Documents/music/vengence/VENGEANCE ESSENTIAL CLUB SOUNDS vol-1/VEC1 Claps/VEC1 Clap 027.wav"}
+                        {:plugin-name "Audio/Generators/VST/Sylenth1" :preset 82}
+                        {:plugin-name "Audio/Generators/VST/Sylenth1" :preset 83}]})
+  (set-patr-0)
+  (set-patr-1)
+  (set-patr-2)
+  )
+; (demo)
 
 (comment
-  (set-notes 1 [[0 60 100 0.5]])
-  (set-notes 1 [[0 66 90 0.5]])
-  (set-notes 1 [[0 60 80 1] [1 62 85 1]])
-  (set-notes 1 [[0 70 80 1] [1 72 85 1] [2 60 80 1] [3 62 85 1]])
-  (set-notes 1 [[0 40 80 1] [1 42 85 1] [2 43 80 1] [3 44 85 1] [4 45 85 1]])
+  (demo)
   
-  (reset-patr 1 4)
-  (reset-patr 1 8)
+  (set-patr 0 4)
+  (set-patr 0 8)
+  (set-notes 1 [[0 48 0.8 0.5]])
   
-  (osc/osc-send *client* "/renoise/evaluate" "clj4rnx.track = renoise.song().patterns[1]:track(1)")
-  (osc/osc-send *client* "/renoise/evaluate" "clj4rnx.line = clj4rnx.track:line(1)")
-  (osc/osc-send *client* "/renoise/evaluate" "clj4rnx.line:note_column(1).note_string = 'C-5'")
-  (osc/osc-send *client* "/renoise/evaluate" "clj4rnx.line:note_column(1).note_string = 'D-6'")
-
-  (osc/osc-send *client* "/renoise/song/bpm" 120)
-  (osc/osc-send *client* "/renoise/song/bpm" 140)
-  (osc/osc-send *client* "/renoise/evaluate" "renoise.song().transport.bpm = 234")
+  (set-notes 1 (take-beats 8.0 (xform-time [0 48 0.8 0.5] [0.0] 1.0)))
+  (set-notes 2 (take-beats 8.0 (xform-time [0 48 0.8 0.5] [1.0] 2.0)))
+  (set-notes 3 (take-beats 8.0 (xform-time [0 48 0.8 0.25] [0.5] 1.0)))
+  
+  (set-notes 1 (take-beats 4.0 (filter (within-beats 0.0 1.0 4.0) (xform-time [0 48 0.8 0.5] [0.0] 1.0))))
+  (set-notes 1 (take-beats 4.0 (filter (within-beats 0.0 0.5 3.0 3.5 4.0) (xform-time [0 48 0.8 0.5] [0.0] 1.0))))
+  (set-notes 1 [[1 48 0.8 0.5]])
+  (set-notes 1 [[0 60 0.8 1] [1 62 0.85 1]])
+  (set-notes 1 [[0 70 0.8 1] [1 72 0.85 1] [2 60 0.8 1] [3 62 0.85 1]])
+  (set-notes 1 [[0 40 0.8 1] [1 42 0.85 1] [2 43 0.8 1] [3 44 0.85 1] [4 45 0.85 1]])
   )
 
