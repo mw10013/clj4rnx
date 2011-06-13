2ce2e04a937492b85b8a2e500593920a58d4d73e
Michael Wu <mw10013@gmail.com>
2011-06-13 01:20:34 -0400

    snapshot

--- src/clj4rnx.clj
+++ src/clj4rnx.clj
@@ -62,7 +62,7 @@
       (ev "clj4rnx.note_column.note_value = " p)
       (ev "clj4rnx.note_column.volume_value = " (math/round (* v 254)))
       (ev "clj4rnx.note_column.instrument_value = " (dec trk-idx))
-      (ev "clj4rnx.note_column = clj4rnx.track:line(" (int (+  l (* d *lpb*))) "):note_column(1)")
+      (ev "clj4rnx.note_column = clj4rnx.track:line(" (int (+  l (* d 4 *lpb*))) "):note_column(1)")
       (ev "clj4rnx.note_column.note_string = 'OFF'" ))))
 
 (defn set-bars [trk-idx bars]
@@ -73,18 +73,6 @@
     (fn [idx bar]
       (map #(assoc %1 :t (+ idx (:t %1))) bar))
     (iterate inc 0)  bars)))
-
-(def n*
-     {
-      :c4w {:p 48 :v 3/4 :d 1}
-      :c4h {:p 48 :v 3/4 :d 1/2}
-      :c4q {:p 48 :v 3/4 :d 1/4}
-      :c4e {:p 48 :v 3/4 :d 1/8}
-      :c4s {:p 48 :v 3/4 :d 1/16}
-      :d4q {:p 50 :v 3/4 :d 1/4}
-      :d4e {:p 50 :v 3/4 :d 1/8}
-      :e4q {:p 52 :v 3/4 :d 1/4}
-      })
 
 ;(map #(re-find #"(([cdefgab])(\d*))?([whqis]*)" %) ["c4" "w" "i" "d4" "e4h"]) 
 
@@ -104,8 +92,6 @@
   (println "notes-from-keyword:" ctx kw)
   (let [n (assoc (note-from-keyword kw) :t (:t ctx))]
     (conj ctx [:t (+ (:t ctx) (:d n))] (when (:p n) [:ns (conj (:ns ctx) n)]))))
-
-(notes-from-keyword {:t 0 :ns [{:n 0}]} :i)
 
 (defn- set-to-notes [ctx coll]
   (println "set-to-notes:" ctx coll)
@@ -141,45 +127,7 @@
 
 (defn- coll-to-bars [coll] (->> coll coll-to-notes notes-to-bars))
 
-; (coll-to-bars [:c4w :c4w :c4e :c4e])
-
-; (coll-to-notes [:c4w :c4w :c4e :c4e])
-; (coll-to-notes [:i :ci :i :ci])
-; (coll-to-notes [#{:c4h [:d4q :e4q :c4q]}])
-; (coll-to-notes [#{:c4h :d4q}])
-; (coll-to-notes [#{[:c4s :c4s :c4s] [:d4e :d4e]}])
-; (def c* (coll-to-notes [:c4w #{[:c4s :c4s :c4s] [:d4e :d4e]}]))
-; (notes-to-bars (coll-to-notes [:c4w :c4w :c4e :c4e]))
-; (notes-to-bars (coll-to-notes [:c4w #{[:c4s :c4s :c4s] [:d4e :d4e]}]))¯
-
-(comment (defn repeat-beats [n-coll dur & args]
-           (let [times (if (first args) (range (first args)) (iterate inc 0))]
-             (mapcat (fn [t] (map #(alter-note %1 1 + %2) n-coll (repeat (* dur t)))) times))))
-
-(defn take-beats [n n-coll]  (take-while #(< (% 0) n) n-coll))
-
-(defn assoc-time- [t-coll dur n-coll]
-  (map #(assoc %1 0 %2) n-coll (mapcat #(map + t-coll (repeat (* dur %))) (iterate inc 0))))
-; (->> *base-note* repeat (assoc-time [0.0] 0.5) (take 5))
-; (take 5 (assoc-time [0.0] 1.0 (repeat *base-note*)))
-
-(def assoc-time (partial map #(assoc %2 0 %1))) ; [t-coll n-coll]
-(def assoc-pitch (partial map #(assoc %2 1 %1))) ; [p-coll n-coll]
-(def assoc-vel (partial map #(assoc %2 2 %1))) ; [vel-coll n-coll]
-(def assoc-dur (partial map #(assoc %2 3 %1))) ; [dur-coll n-coll]
-(def assoc-tp (partial map (fn [tp n] (-> n (assoc 0 (tp 0)) (assoc 1 (tp 1)))))) ; [tp-coll n-coll]
-
-(defn assoc-bars [bars]
-  (mapcat 
-   (fn [idx bar]
-     (map #(assoc %1 0 (+ (* 4 idx) (%1 0))) bar))
-   (iterate inc 0) (cycle bars)))
-
-; (take 7 (assoc-bars [[[0 48] [3/2 48]] [[0 46] [3/2 46]]]))
-
-; [[[0 48] [3/2 48]] [[0 46] [3/2 46]]]
-; (take 5 (map-indexed #(vector %1 %2) [[[0 48] [3/2 48]] [[0 46] [3/2 46]]]))
-; (take 5 (map-indexed #(assoc %2 0 %1) [[[0 48] [3/2 48]] [[0 46] [3/2]46]]]))
+(defn- coll-to-infinite-bars [coll] (->> coll coll-to-bars repeat (mapcat identity)))
 
 (defn within-beats [l u & args]
   (let [len (if (odd? (count args)) (last args))
@@ -191,51 +139,27 @@
 
 ;  (take 8 (filter (within-beats 1/8 2) [[0 0] [1/8 1/8] [1/4 1/4] [2 2] [3 3]]))
 
-(def note-fs* (ref {}))
-(defn add-note-f [name f] (dosync (alter note-fs* assoc name f)) nil)
-(defn get-notes [name] ((@note-fs* name)))
-(defn take-notes [name beat-cnt] (->> name get-notes (take-beats beat-cnt)))
-
-(add-note-f :qtr #(->> [0 48 3/4 1] repeat (assoc-time (iterate inc 0))))
-(add-note-f :six #(->> (get-notes :qtr) (assoc-time (iterate (partial + 1/4) 0)) (assoc-dur (repeat 1/4))))
-(add-note-f :off-eighths #(->> (get-notes :qtr) (assoc-time (iterate inc 1/2)) (assoc-dur (repeat 1/4))))
-(add-note-f :one-three #(->> (get-notes :qtr) (assoc-time (iterate (partial + 2) 1))))
-(add-note-f :bass-1 #(->> (get-notes :off-eighths) (assoc-pitch (cycle [48 48 50 50 52 52 55 55]))))
-(add-note-f :bass-1a #(->> (get-notes :off-eighths) (assoc-tp [[0 48] [1/4 46] [1 48] [5/4 48]])))
-
 (def bar-fs* (ref {}))
 (defn add-bar-f [name f] (dosync (alter bar-fs* assoc name f)) nil)
 (defn get-bars [name] ((@bar-fs* name)))
 
-; what about dur.
-(def *base-note* [0 48 3/4 1])
-(add-bar-f :whl #(repeat [*base-note*]))
-;(add-bar-f :qtr #(repeat (map (fn [b] (assoc *base-note* 0 b)) (range 4))))
-(add-bar-f :qtr #(mapcat identity (repeat (coll-to-bars [:i :ci :i :ei :f]))))
-(add-bar-f :eth #(repeat (map (fn [b] (assoc *base-note* 0 (* b 1/2))) (range 8))))
-(add-bar-f :off-eth #(repeat (map (fn [b] (assoc *base-note* 0 (+ b 1/2))) (range 4))))
-(add-bar-f :six #(repeat (map (fn [b] (assoc *base-note* 0 (* b 1/4))) (range 16))))
-(add-bar-f :bass-1 #(cycle [[[0 48 3/4 1] [3/2 48 3/4 1/2]]]))
+(add-bar-f :qtr #(coll-to-infinite-bars [:c :c :c :c]))
+(add-bar-f :off-qtr #(coll-to-infinite-bars [:q :c :q :c]))
+(add-bar-f :off-eth #(coll-to-infinite-bars [:i :ci :i :ci :i :ci :i :ci]))
+(add-bar-f :bass-1 #(coll-to-infinite-bars [:i :c3i :i :c3i :i :c3i :i :c3i :i :g2i :i :g2i :i :g2i :i :g2i :i]))
 
 ; (set-patr :grv-1 0 2)
-; (take 2 (get-bars :bass-1))
+; (take 2 (get-bars :qtr))
 
 (def patr-fs* (ref {}))
 (defn add-patr-f [name f] (dosync (alter patr-fs* assoc name f)))
 (defn get-patr [name] ((@patr-fs* name)))
 
-(comment (add-patr-f :grv-1 (fn []
-                              {:bd (get-bars :whl)
-                               :sd (get-bars :qtr)
-                               :hh-c (get-bars :off-eth)
-                               :bass (get-bars :bass-1)
-                               })))
-
 (add-patr-f :grv-1 (fn []
                        {:bd (get-bars :qtr)
-;                        :sd (get-bars :qtr)
-;                        :hh-c (get-bars :off-eth)
-;                        :bass (get-bars :bass-1)
+                        :sd (get-bars :off-qtr)
+                        :hh-c (get-bars :off-eth)
+                        :bass (get-bars :bass-1)
                         }))
 
 (add-patr-f :grv-0 (fn []
@@ -249,9 +173,9 @@
   (ev "clj4rnx.pattern.number_of_lines = " (* bar-cnt 4 *lpb*))
   (let [patr (get-patr name)]
     (set-bars 1 (take bar-cnt (:bd patr)))
-;    (set-bars 2 (take bar-cnt (:sd patr)))
-;    (set-bars 3 (take bar-cnt (:hh-c patr)))
-;    (set-bars 5 (take bar-cnt (:bass patr)))
+    (set-bars 2 (take bar-cnt (:sd patr)))
+    (set-bars 3 (take bar-cnt (:hh-c patr)))
+    (set-bars 5 (take bar-cnt (:bass patr)))
     ))
 
 (def patr-specs* [{:patr :grv-0 :bar-cnt 2} {:patr :grv-1 :bar-cnt 2}])
@@ -268,35 +192,7 @@
                         {:sample-filename "/Users/mw/Documents/music/vengence/VENGEANCE ESSENTIAL CLUB SOUNDS vol-1/VEC1 Claps/VEC1 Clap 027.wav"}
                         {:plugin-name "Audio/Generators/VST/Sylenth1" :preset 87}
                         {:plugin-name "Audio/Generators/VST/Sylenth1" :preset 83}]})
-                                        ;  (set-patr-0 4)
-  (set-patrs patr-specs*)
-  )
+  (set-patrs patr-specs*))
+
 ; (demo)
 
-(def k*
-     {
-      :1w "1w"
-      :1h"1h"
-      :1q "1q"
-      :1e "1e"
-      :5w "5w"
-      :5h"5h"
-      :5q "5q"
-      :5e "5e"
-      :-1w "-1w"
-      :-1h"-1h"
-      :-1q "-1q"
-      :-1e "-1e"
-      })
-
-[:1q :1q :1q :1q]
-['(ch :-1w :1w :5q) :6q]
-['(ch :-1w :1w :5e) :6q :2e]
-['(ch :-1w :1q :5e) '(ch :6q :0e) :2e]
-; sometimes vertical, sometimes horizontal
-; jfugue uses + to indicate vertical, - to indicate ties
-[:-1w+1q+5e ]
-
-
-; :c4www.v3:4
-(map #(re-find #"([cdefgab])(\d*)([whqes]*)" %) ["c4wwww" "c" "cw" "cwwwq"])
